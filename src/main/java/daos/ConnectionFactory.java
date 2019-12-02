b782035707f31fbf63de24e847845f9a00df8c3e
kendrang <kendrang@users.noreply.github.com>
2019-12-02 08:54:36 -0500

    done

--- src/main/java/daos/ConnectionFactory.java
+++ src/main/java/daos/ConnectionFactory.java
@@ -1,24 +1,24 @@
 package daos;
 
-import com.mysql.jdbc.Driver;
-
+import com.mysql.cj.jdbc.Driver;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
         public static final String URL = "jdbc:mysql://localhost:3306/cars";
-        public static final String USER = "kendra";
+        public static final String USER = "root";
         public static final String PASS = "zipcode78";
         /**
          * Get a connection to database
          * @return Connection object
          */
-        public static ConnectionFactory getConnection()
+        public static Connection getConnection()
         {
             try {
                 DriverManager.registerDriver(new Driver());
-                return (ConnectionFactory) DriverManager.getConnection(URL, USER, PASS);
+                return DriverManager.getConnection(URL, USER, PASS);
             } catch (SQLException ex) {
                 throw new RuntimeException("Error connecting to the database", ex);
             }
@@ -27,7 +27,7 @@ public class ConnectionFactory {
          * Test Connection
          */
         public static void main(String[] args) {
-            daos.ConnectionFactory connectionFactory = ConnectionFactory.getConnection();
+            ConnectionFactory.getConnection();
         }
     }
 
